99c3c4f05c1a892640f94a1cae26dda028daa834
juansimonmontoro <montorosimon34@gmail.com>
2022-07-14 23:38:02 -0300

    video 7

--- Portfolio/src/app/app.module.ts
+++ Portfolio/src/app/app.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
 import { SocialComponent } from './components/social/social.component';
 import { AboutComponent } from './components/about/about.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { ExperencieComponent } from './components/experencie/experencie.component';
+import { EducationComponent } from './components/education/education.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { HardAndSoftSkillsComponent } from './components/hard-and-soft-skills/hard-and-soft-skills.component';
+import { ProjectComponent } from './components/project/project.component';
+
+
+
 
 @NgModule({
   declarations: [
@@ -14,11 +22,19 @@ import { AboutComponent } from './components/about/about.component';
     HeaderComponent,
     LogoAPComponent,
     SocialComponent,
-    AboutComponent
+    AboutComponent,
+    BannerComponent,
+    ExperencieComponent,
+    EducationComponent,
+    HardAndSoftSkillsComponent,
+    ProjectComponent
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({})
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Portfolio/src/app/components/experencie/experencie.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-experencie',
+  templateUrl: './experencie.component.html',
+  styleUrls: ['./experencie.component.css']
+})
+export class ExperencieComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
