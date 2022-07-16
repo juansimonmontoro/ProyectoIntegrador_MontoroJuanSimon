884d2c8bae048909ec9ea97a7207033a6995a7c6
juansimonmontoro <montorosimon34@gmail.com>
2022-07-15 22:23:58 -0300

    Creacion de carpeta FrontEnd

--- Portfolio/FrontEnd/src/app/app.module.ts
+++ Portfolio/FrontEnd/src/app/app.module.ts
@@ -4,7 +4,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
-import { SocialComponent } from './components/social/social.component';
 import { AboutComponent } from './components/about/about.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { ExperencieComponent } from './components/experencie/experencie.component';
@@ -12,6 +11,7 @@ import { EducationComponent } from './components/education/education.component';
 import { NgCircleProgressModule } from 'ng-circle-progress';
 import { HardAndSoftSkillsComponent } from './components/hard-and-soft-skills/hard-and-soft-skills.component';
 import { ProjectComponent } from './components/project/project.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 
@@ -21,13 +21,13 @@ import { ProjectComponent } from './components/project/project.component';
     AppComponent,
     HeaderComponent,
     LogoAPComponent,
-    SocialComponent,
     AboutComponent,
     BannerComponent,
     ExperencieComponent,
     EducationComponent,
     HardAndSoftSkillsComponent,
-    ProjectComponent
+    ProjectComponent,
+    FooterComponent
 
   ],
   imports: [
