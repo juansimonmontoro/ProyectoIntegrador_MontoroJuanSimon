673c72130cd3cf12c7133619d0d9fb54a0511aa9
juansimonmontoro <montorosimon34@gmail.com>
2022-08-03 20:10:28 -0300

    16

--- Portfolio/BackEnd/src/main/java/com/portfolio/jsm/Controller/PersonaController.java
+++ Portfolio/BackEnd/src/main/java/com/portfolio/jsm/Controller/PersonaController.java
@@ -59,7 +59,7 @@ public class PersonaController {
 
     @GetMapping("/personas/traer/perfil")
     public Persona findPersona() {
-        return ipersonaService.findPersona((long)6);
+        return ipersonaService.findPersona((long)1);
     }
 
 }
